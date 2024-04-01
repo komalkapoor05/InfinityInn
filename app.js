8dca38c998e550456793bd77345b94877f47cda8
Komal Kapoor <kapoorkomal2020@gmail.com>
2024-04-02 04:16:28 +0530

    Enhanced footer design

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/InfinityInn";
-const dbUrl = process.env.ATLASDB_URL;
+const MONGO_URL = "mongodb://127.0.0.1:27017/InfinityInn";
+// const dbUrl = process.env.ATLASDB_URL;
 
 main()
   .then(() => {
@@ -32,7 +32,7 @@ main()
   });
 
 async function main() {
-  await mongoose.connect(dbUrl);
+  await mongoose.connect(MONGO_URL);
 }
 
 app.set("view enginie", "ejs");
@@ -43,7 +43,7 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 const store = MongoStore.create({
-  mongoUrl: dbUrl,
+  mongoUrl: MONGO_URL,
   crypto: {
     secret: process.env.SECRET,
   },
@@ -89,6 +89,10 @@ app.use("/", userRouter);
 
 app.get("/", (req, res) => {
   res.redirect("/listings");
+});
+
+app.get("/about-us", (req, res) => {
+  res.render("../views/others/about_us.ejs");
 });
 
 app.all("*", (req, res, next) => {
